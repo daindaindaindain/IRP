4f8b142842455eeaf510ee79048aa5c1865b354b
DESKTOP-VLPQ44Q\MALIFE <tkfkdt@gmail.com>
2023-08-31 10:13:27 +0900

    2번째 커밋 테스트 입니다.

--- src/com/test/User.java
+++ src/com/test/User.java
@@ -4,7 +4,10 @@ public class User {
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-
+       long num= 100_0000_0000L;
+       System.out.println(num);
+       
+       
     }
 
 }
